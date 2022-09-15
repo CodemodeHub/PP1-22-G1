ae40053493477a0c7f9129ea66f252f3d1de4b38
Adil <adilzhapar2002@gmail.com>
2022-09-15 19:40:18 +0600

    arithmetic

---
+++ w1/part2/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    int age;
+    cin >> name >> age;
+
+    // cout << name << " " << age << endl; 
+    // cout << name << "." << age << endl; 
+    // cout << "My name is: " << name << ", and my age is: " << age << endl;
+    cout << age + 1000 << endl;
+    
+    return 0;
+}
