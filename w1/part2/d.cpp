28b63c86636a9110f1387670924c62fef800b760
Adil <adilzhapar2002@gmail.com>
2022-09-15 20:37:41 +0600

    math functions

--- w1/part2/d.cpp
+++ w1/part2/d.cpp
@@ -6,7 +6,15 @@ int main(){
     string a = "Yas";
     string b = "lan";
 
-    cout << a + "!!!" + b;
+    cout << a + "!!!" + b << endl;
+
+
+    // ASCII
+    /*
+    cout << char(65) << endl;
+    cout << int('A') << endl;
+    */
+    
 
     return 0;
 
---
+++ w1/part2/e.cpp
@@ -0,0 +1,18 @@
+// Math functions
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << "root of n: " << sqrt(n) << endl;
+    cout << "abs of n: " << abs(n) << endl;
+
+    cout << "Max between 18 and n: " << max(18, n) << endl;
+
+    return 0;
+}
