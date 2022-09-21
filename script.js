6a92c5da7414dac579c8ad5bf03bd436b4c20c67
Adil <adilzhapar2002@gmail.com>
2022-09-21 16:03:50 +0600

    undefined

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const execSync = require('child_process').execSync;
 const mssg = process.argv[2];
 execSync('git add . && git commit -m \"' + mssg + '\" && git push', { stdio:[0, 1, 2] })
+// npm run push -- "msg"
