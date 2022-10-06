29eed6a0d7c19b4a4c2b29db18066b504de4744f
Adil <adilzhapar2002@gmail.com>
2022-10-06 20:02:03 +0600

    double array

---
+++ w4/p2/b.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+    cin >> n;
+    int arr[n][n];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int maxx = INT_MIN;
+    int second = INT_MIN;
+    
+    
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(arr[i][j] > maxx){
+                maxx = arr[i][j];
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(arr[i][j] > second && arr[i][j] < maxx){
+                second = arr[i][j];
+            }
+
+        }
+    }
+    if(second == INT_MIN){
+        cout << 0 << endl;
+    }else{
+        cout << second << endl;
+
+    }
+
+    return 0;
+}
