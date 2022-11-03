e88c55bd6e65eefd8677bca80a857feca5561bda
Adil <adilzhapar2002@gmail.com>
2022-11-03 19:12:50 +0600

    lab-6-c

---
+++ w8/p2/a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int getEqual(int a[], int b[], int n){
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(a[i] == b[j]){
+                cnt++;
+                b[j] = -1;
+                break;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int a[n], b[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        cin >> b[i];
+    }
+
+    int res = getEqual(a, b, n);
+
+    cout << res << endl;
+
+    return 0;
+}
