1dc350f62a0256c4be4da9b2f5e779c5fc295685
Adil <adilzhapar2002@gmail.com>
2022-12-08 19:39:11 +0600

    answer for tasl

--- w13/p2/task1.cpp
+++ w13/p2/task1.cpp
@@ -32,3 +32,20 @@ int main(){
     
     return 0;
 }
+
+// ANSWER
+// Iniesta 30 40
+// Yaslan 30 2
+// Kairosh 25 25
+// Neymar 25 17
+// Messi 13 7
+// Ronaldo 12 8
+// Alikhan 3 24
+// -------
+// Iniesta 40 35.00
+// Kairosh 30 25.00
+// Neymar 33 21.00
+// Yaslan 18 16.00
+// Alikhan 19 13.50
+// Messi 34 10.00
+// Ronaldo 37 10.00
